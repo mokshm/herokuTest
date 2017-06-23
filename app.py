cd8adddacdc98d8019a645bf6dc7b8658312fc5b
Moksh Makhija <moksh.mcs.du.2015@gmail.com>
2017-06-24 03:25:33 +0530

    Basic Skeleton of heroku app, hope it works

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__);
+
+@app.route('/',methods=['GET','POST'])
+def index():
+  print("<h2>Hello World</h2>");
+
+
+if __name__ == '__main__':
+  app.run(debug=True);
